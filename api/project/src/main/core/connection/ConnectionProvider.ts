39c7f985f4028d9b992af7b7618088873a0b6177
kurobanekengo <kurobanekengo@gmail.com>
2021-03-05 02:42:51 +0900

    ConnectionをSingletonで保持するように修正

--- api/project/src/main/core/connection/ConnectionProvider.ts
+++ api/project/src/main/core/connection/ConnectionProvider.ts
@@ -4,38 +4,44 @@ import { Client } from "pg";
 import { injectable } from "inversify";
 import { RootContext } from "@core/RootContext";
 
-const getConnection = (): Client => {
-  const dbConfigPath = `${RootContext.apiRootDir}/database.yml`;
-  console.log(dbConfigPath);
-  const dbConfigValue = fs.readFileSync(dbConfigPath, "utf8");
-  const dbConfig = yaml.load(dbConfigValue);
-
-  const host = dbConfig["common"]["host"];
-  const user = dbConfig["common"]["username"];
-  const password = dbConfig["common"]["username"];
-  // TODO: 環境に応じて切り替えること
-  const database = dbConfig["development"]["database"];
-
-  //   console.log("----- database access information -----");
-  //   console.log(`host: ${host}`);
-  //   console.log(`username: ${user}`);
-  //   console.log(`password: ${password}`);
-  //   console.log(`database : ${database}`);
-
-  const client = new Client({
-    host,
-    user,
-    password,
-    database,
-    port: 5432,
-  });
-
-  return client;
-};
-
 @injectable()
 export class ConnectionProvider {
+  private connection: Client;
+
+  public constructor() {
+    this.connection = this.createConnection();
+  }
+
   public getConnection() {
-    return getConnection();
+    return this.connection;
+  }
+
+  private createConnection(): Client {
+    const dbConfigPath = `${RootContext.apiRootDir}/database.yml`;
+    console.log(dbConfigPath);
+    const dbConfigValue = fs.readFileSync(dbConfigPath, "utf8");
+    const dbConfig = yaml.load(dbConfigValue);
+
+    const host = dbConfig["common"]["host"];
+    const user = dbConfig["common"]["username"];
+    const password = dbConfig["common"]["username"];
+    // TODO: 環境に応じて切り替えること
+    const database = dbConfig["development"]["database"];
+
+    // console.log("----- database access information -----");
+    // console.log(`host: ${host}`);
+    // console.log(`username: ${user}`);
+    // console.log(`password: ${password}`);
+    // console.log(`database : ${database}`);
+
+    const client = new Client({
+      host,
+      user,
+      password,
+      database,
+      port: 5432,
+    });
+
+    return client;
   }
 }

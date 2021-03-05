ee890a684bcd62f80fd9eb94216894b4f3591675
kurobanekengo <kurobanekengo@gmail.com>
2021-03-05 22:53:16 +0900

    削除したファイルのimportが残っていたので修正

--- api/project/main.ts
+++ api/project/main.ts
@@ -2,9 +2,6 @@ import { TYPES } from "@core/container/types";
 import { getContainer } from "@core/container/getContainer";
 import { NendoService } from "@services/nendo/NendoService";
 import { ConnectionProvider } from "@core/connection/ConnectionProvider";
-import "@services/extensions/getTemplate.extension";
-import "@services/extensions/getConnection.extension";
-import "@services/extensions/withQuery.extension";
 
 const container = getContainer();
 

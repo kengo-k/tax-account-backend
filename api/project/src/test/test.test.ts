5613b8b95be4cc62dcdfb0a69aa0972e4c47111f
kurobanekengo <kurobanekengo@gmail.com>
2021-03-06 16:37:31 +0900

    API疎通テスト

--- api/project/src/test/test.test.ts
+++ api/project/src/test/test.test.ts
@@ -2,5 +2,8 @@ import { testServer } from "@test/testServer";
 
 test("test", async () => {
   await testServer.start();
+  const client = testServer.getClient();
+  const result = await client.post("/api/v1/journal");
+  expect(result.data).toEqual({ result: 5 });
   testServer.stop();
 });

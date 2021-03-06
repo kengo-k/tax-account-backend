5c6f5c620805f0374d06cb6a045550ee43b7fce8
kurobanekengo <kurobanekengo@gmail.com>
2021-03-06 18:07:36 +0900

    insert実行後に追加された新規IDを返せるようにする

--- api/project/src/main/services/BaseService.ts
+++ api/project/src/main/services/BaseService.ts
@@ -117,6 +117,7 @@ export abstract class BaseService {
     const sqlString = `insert into
 ${tableName}(${keys.join(",")})
 values(${keys.map(getParam)})
+returning id
 `;
     const connection = this.getConnection();
     console.log(sqlString);

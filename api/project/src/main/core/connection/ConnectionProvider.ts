f1d43c62becf9edadcbbb053b6f0487ca2bc86a8
kurobanekengo <kurobanekengo@gmail.com>
2021-03-07 17:43:53 +0900

    コメント整備

--- api/project/src/main/core/connection/ConnectionProvider.ts
+++ api/project/src/main/core/connection/ConnectionProvider.ts
@@ -1,11 +1,13 @@
 import * as fs from "fs";
 import * as yaml from "js-yaml";
-import { Client } from "pg";
 import { injectable } from "inversify";
 import { RootContext } from "@core/RootContext";
 const postgres = require("postgres");
 
-class ConnectionWrapper {
+/**
+ * DBアダプター毎の実装差異を吸収するためのコネクションのラッパー
+ */
+export class ConnectionWrapper {
   public constructor(private realConnection: any) {}
   public close() {
     this.realConnection.end();
@@ -33,20 +35,14 @@ export class ConnectionProvider {
     const dbConfig: any = yaml.load(dbConfigValue);
 
     const host = dbConfig["common"]["host"];
-    const user = dbConfig["common"]["username"];
+    const username = dbConfig["common"]["username"];
     const password = dbConfig["common"]["username"];
     // TODO: 環境に応じて切り替えること
     const database = dbConfig["development"]["database"];
 
-    // console.log("----- database access information -----");
-    // console.log(`host: ${host}`);
-    // console.log(`username: ${user}`);
-    // console.log(`password: ${password}`);
-    // console.log(`database : ${database}`);
-
     const connection = postgres({
       host,
-      username: user,
+      username,
       password,
       database,
       port: 5432,

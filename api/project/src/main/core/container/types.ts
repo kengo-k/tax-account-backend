ffaf678b1b5cfd21430ec613226a2edb54066b49
kurobanekengo <kurobanekengo@gmail.com>
2021-04-15 15:27:28 +0900

    #5 累積計算

--- api/project/src/main/core/container/types.ts
+++ api/project/src/main/core/container/types.ts
@@ -2,8 +2,12 @@ const TYPES = {
   // Connection
   ConnectionProvider: Symbol.for("ConnectionProvider"),
   // Controllers&Services
-  JournalService: Symbol.for("JournalService"),
+  // Journal
   JournalController: Symbol.for("JournalController"),
+  JournalService: Symbol.for("JournalService"),
+  // Master
+  MasterController: Symbol.for("MasterController"),
+  SaimokuService: Symbol.for("SaimokuService"),
   NendoService: Symbol.for("NendoService"),
 };
 

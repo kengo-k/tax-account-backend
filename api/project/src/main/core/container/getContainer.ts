2b7b4a2c15d9799f8012a36ae6c1d4d1c734862f
kurobanekengo <kurobanekengo@gmail.com>
2021-04-15 15:27:28 +0900

    #5 累積計算

--- api/project/src/main/core/container/getContainer.ts
+++ api/project/src/main/core/container/getContainer.ts
@@ -3,8 +3,10 @@ import { Container } from "inversify";
 import { TYPES } from "@core/container/types";
 import { ConnectionProvider } from "@core/connection/ConnectionProvider";
 import { JournalService } from "@services/journal/JournalService";
+import { SaimokuService } from "@services/master/SaimokuService";
 import { NendoService } from "@services/nendo/NendoService";
 import { JournalController } from "@controllers/journal/JournalController";
+import { MasterController } from "@controllers/master/MasterController";
 
 // prettier-ignore
 const createContainer = () => {
@@ -12,6 +14,8 @@ const createContainer = () => {
   container.bind<ConnectionProvider>(TYPES.ConnectionProvider).to(ConnectionProvider).inSingletonScope();
   container.bind<JournalController>(TYPES.JournalController).to(JournalController);
   container.bind<JournalService>(TYPES.JournalService).to(JournalService);
+  container.bind<MasterController>(TYPES.MasterController).to(MasterController);
+  container.bind<SaimokuService>(TYPES.SaimokuService).to(SaimokuService);
   container.bind<NendoService>(TYPES.NendoService).to(NendoService);
   return container;
 }
--- api/project/src/main/services/journal/JournalService.ts
+++ api/project/src/main/services/journal/JournalService.ts
@@ -5,20 +5,46 @@ import { ConnectionProvider } from "@core/connection/ConnectionProvider";
 import { LedgerSearchRequest } from "@common/model/journal/LedgerSearchRequest";
 import { LedgerSearchResponse } from "@common/model/journal/LedgerSearchResponse";
 import { selectLedger as getSelectLedgerSql } from "./selectLedger.sql";
+import { SaimokuService } from "@services/master/SaimokuService";
 
 @injectable()
 export class JournalService extends BaseService {
   public constructor(
     @inject(TYPES.ConnectionProvider)
-    public connectionProvider: ConnectionProvider
+    public connectionProvider: ConnectionProvider,
+    @inject(TYPES.SaimokuService)
+    public saimokuService: SaimokuService
   ) {
     super();
   }
 
   public async selectLedger(condition: LedgerSearchRequest) {
-    return await this.select(
+    const saimokuDetail = (
+      await this.saimokuService.selectSaimokuDetail({
+        saimoku_cd: condition.target_cd,
+      })
+    )[0];
+    return await this.mapSelect(
       LedgerSearchResponse,
-      getSelectLedgerSql(condition)
+      getSelectLedgerSql(condition),
+      (res) => {
+        const sumL = res.karikata_sum;
+        const sumR = res.kasikata_sum;
+        if (saimokuDetail.kamoku_bunrui_type === "L") {
+          if ((res.karikata_cd = condition.target_cd)) {
+            res.acc = sumL - sumR;
+          } else {
+            res.acc = sumR - sumL;
+          }
+        } else {
+          if ((res.karikata_cd = condition.target_cd)) {
+            res.acc = sumR - sumL;
+          } else {
+            res.acc = sumL - sumR;
+          }
+        }
+        return res;
+      }
     );
   }
 }

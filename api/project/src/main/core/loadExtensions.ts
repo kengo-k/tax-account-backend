fc82b60cac2de10f575c7307d63a485dd8fcc371
kurobanekengo <kurobanekengo@gmail.com>
2021-04-20 15:04:19 +0900

    test時もextensionsを読み込めるように外出し

---
+++ api/project/src/main/core/loadExtensions.ts
@@ -0,0 +1,6 @@
+import "@services/extensions/create.extension";
+import "@services/extensions/select.extension";
+import "@services/extensions/selectById.extension";
+import "@services/extensions/update.extension";
+import "@services/extensions/delete.extension";
+export {};

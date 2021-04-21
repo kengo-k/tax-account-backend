260351a1db085d66cbdc26c46e2e9bd25c72b48c
kurobanekengo <kurobanekengo@gmail.com>
2021-04-21 10:49:20 +0900

    #29 初期データ取得APIの実装

--- api/project/src/main/controllers/presentation/PresentationController.ts
+++ api/project/src/main/controllers/presentation/PresentationController.ts
@@ -3,26 +3,48 @@ import { TYPES } from "@core/container/types";
 import { MasterService } from "@services/master/MasterService";
 import { BaseController } from "@controllers/BaseController";
 import { InitSearchResponse } from "@common/model/presentation/InitSearchResponse";
+import { InitSearchRequest } from "@common/model/presentation/InitSearchRequest";
+import { RequestError } from "@common/error/ApplicationError";
+import { LedgerSearchResponse } from "@common/model/journal/LedgerSearchResponse";
+import { JournalService } from "@services/journal/JournalService";
 
 @injectable()
 export class PresentationController extends BaseController {
   public constructor(
     @inject(TYPES.MasterService)
-    public masterService: MasterService
+    public masterService: MasterService,
+    @inject(TYPES.JournalService)
+    public journalService: JournalService
   ) {
     super();
   }
 
   public selectInit(req: any, res: any) {
     this.execute(req, res, async () => {
+      const [param] = InitSearchRequest.isValid(req.query);
+      if (param == null) {
+        // prettier-ignore
+        throw new RequestError(`invalid request: create for ${JSON.stringify(req.body)}`);
+      }
       const nendoList = await this.masterService.selectNendoList();
+      let targetNendo = nendoList[0].nendo;
+      if (param.nendo != null) {
+        targetNendo = param.nendo;
+      }
+      let ledgerList: LedgerSearchResponse[] = [];
+      if (param.target_cd != null) {
+        ledgerList = await this.journalService.selectLedger({
+          nendo: targetNendo,
+          target_cd: param.target_cd,
+        });
+      }
       const kamokuMasterList = await this.masterService.selectKamokuList();
       const saimokuMasterList = await this.masterService.selectSaimokuList();
       const result: InitSearchResponse = {
         nendoList,
         kamokuMasterList,
         saimokuMasterList,
-        ledgerList: [],
+        ledgerList,
       };
       return result;
     });

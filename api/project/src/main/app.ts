ecb9712e12c9ab1070a973c6ecd1a87f5b2ca112
kuronia <kuronyanchannel@gmail.com>
2021-05-03 16:50:26 +0900

    #48 許可するHTTPメソッドを明示する

--- api/project/src/main/app.ts
+++ api/project/src/main/app.ts
@@ -6,6 +6,7 @@ import "@core/loadExtensions";
 // CORS対応
 const allowCrossDomain = (req: any, res: any, next: any) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"

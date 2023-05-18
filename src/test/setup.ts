809230ccca526d83b87a85d10252e80dbc55d5e5
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-05-18 20:52:26 +0900

    setup.ts: テストをパスするように修正

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,8 +1,10 @@
 import "tsconfig-paths/register";
 //import * as child_process from "child_process";
-//import { ApplicationContext, Env } from "@core/ApplicationContext";
+import { ApplicationContext, Env } from "@core/ApplicationContext";
 import { testServer } from "@test/testServer";
 
+import * as fs from "fs";
+import * as path from 'path';
 const { execSync } = require('child_process')
 
 export default async function () {
@@ -11,14 +13,18 @@ export default async function () {
   execSync('npx prisma migrate reset --force')
   execSync('npx prisma migrate dev')
 
-  // // テスト環境に切り替える
-  // ApplicationContext.setEnv(Env.test);
-  // // コネクションを生成しておく
-  // testServer.getConnection();
-  // // テスト前にDBを再作成する
-  // child_process.execSync("sh ./init_test.sh");
-  // // 空のDBに初期データを設定する
-  // child_process.execSync("sh ./import_test.sh init");
+  // テスト環境に切り替える
+  ApplicationContext.setEnv(Env.test);
+  // コネクションを生成しておく
+  testServer.getConnection();
+  // TODO ユーザ名とパスワードは直書きをやめること(database.yml経由で取得する)
+  const files = fs.readdirSync(ApplicationContext.dataDir);
+  files.forEach(fileName => {
+    const tableName = path.basename(fileName, ".csv");
+    const filePath = `${ApplicationContext.dataDir}/${fileName}`;
+    execSync(`psql -U root -d test -h account_db -c "\\copy ${tableName} FROM '${filePath}' DELIMITER ',' CSV HEADER"`);
+  });
+
   // express起動
   await testServer.start();
 }

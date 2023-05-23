0beaf6b4ff0f3502d006e4de7167cb8e06b21bed
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-05-23 22:42:33 +0900

    不要になったtest用スクリプトを削除

--- import_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# test用データインポートスクリプト
-cd ./migration
-rake data:import[test,data/$1] 2>/dev/null
--- init_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# test用データインポートスクリプト
-cd ./migration
-rake db:init[test] 2>/dev/null
-rake migrate:run[test] 2>/dev/null
